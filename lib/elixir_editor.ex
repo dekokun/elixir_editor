20d8b5e392c74d7a2a668fecf20d7b7b9f61a341
dekokun <shintaro.kurachi@gmail.com>
2015-05-22 00:25:40 +0900

    elixir_editorが一旦compileを通るように

--- lib/elixir_editor.ex
+++ lib/elixir_editor.ex
@@ -1,2 +1,43 @@
 defmodule ElixirEditor do
+  def start(_type, _args) do
+
+    :ets.new(:message_wall, [:set, :named_table, :public])
+    dispatch = :cowboy_router.compile([
+      {:_, [
+        # cowboy_staticはパスマッチに対して、静的ファイルを読み込む
+        # index.htmlを読み込む
+        {"/", :cowboy_static, {:file, :filename.join(
+          [:filename.dirname(:code.which(__MODULE__)),
+            "..", "priv", "index.html"])}},
+        {"/bundle.js", :cowboy_static, {:file, :filename.join(
+          [:filename.dirname(:code.which(__MODULE__)),
+            "..", "priv", "bundle.js"])}},
+        {"/flex.css", :cowboy_static, {:file, :filename.join(
+          [:filename.dirname(:code.which(__MODULE__)),
+            "..", "priv", "flex.css"])}},
+        # /websocketのリクエストをws_handlerに渡す
+        {"/websocket", :MessageWallHandler, []}
+      ]}
+    ])
+    {:ok, _} = :cowboy.start_http(:http, 100,
+      [{:port, port()}],
+      [
+        {:env, [{:dispatch, dispatch}]}
+      ])
+    :message_wall_sup.start_link()
+  end
+
+  def stop(_State) do
+    :ok
+  end
+
+  def port() do
+    case :os.getenv("PORT") do
+      false ->
+        {:ok, Port} = :application.get_env(:http_port)
+        Port
+      other ->
+        :erlang.list_to_integer(other)
+    end
+  end
 end
