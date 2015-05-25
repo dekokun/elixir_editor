88c1688d11e315d9e9f1a5e26d31ac893ca3333e
dekokun <shintaro.kurachi@gmail.com>
2015-05-25 22:27:55 +0900

    elixir的なコードを目指して変数名を小文字にした

--- lib/message_wall_handler.ex
+++ lib/message_wall_handler.ex
@@ -6,43 +6,43 @@ defmodule MessageWallHandler do
   end
 
   # websocket_init はwebsocket接続が開始された時に実行されます
-  def websocket_init(_, Req, _Opts) do
+  def websocket_init(_, req, _opts) do
     # プロセスをgproc pubsubに登録する
     :gproc_ps.subscribe(:l, :new_message)
     # stateを設定する
-    Ip = get_ip(Req)
-    {UserAgent, _Req} = :cowboy_req.header(<<"user-agent">>, Req)
-    state = %State{ip: Ip, ua: UserAgent, room_id: 1}
+    ip = get_ip(req)
+    {UserAgent, _Req} = :cowboy_req.header(<<"user-agent">>, req)
+    state = %State{ip: ip, ua: UserAgent, room_id: 1}
     # WebSocketリクエストは長くなる可能性があるため
     # 不要なデータをReqから削除
-    Req2 = :cowboy_req.compact(Req)
+    Req2 = :cowboy_req.compact(req)
     # 自動切断を10分に設定する（60万ミリ秒）
     {:ok, Req2, state, 600000, :hibernate}
   end
 
   # get_markdownメッセージの場合はメッセージのリストを返します
-  def websocket_handle({:text, <<"\"get_markdown\"">>}, Req, state) do
+  def websocket_handle({:text, <<"\"get_markdown\"">>}, req, state) do
     room_id = state[:room_id]
     :io.format("get_markdownですよ~n")
     # 最新のメッセージを取得する
     Tuple = get_markdown(room_id)
     # メッセージをJiffyが変換できる形式に変更
-    Markdown = format_markdown(Tuple)
-    :io.format("dataですよ ~w~n", [Markdown])
+    markdown = format_markdown(Tuple)
+    :io.format("dataですよ ~w~n", [markdown])
     # JiffyでJsonレスポンスを生成
-    JsonResponse = :jiffy.encode(%{
+    jsonResponse = :jiffy.encode(%{
       <<"type">> => <<"all">>,
-      <<"markdown">> => Markdown
+      <<"markdown">> => markdown
     })
-    :io.format("responceですよ ~s~n", [JsonResponse])
+    :io.format("responceですよ ~s~n", [jsonResponse])
     # JSONを返す
-    {:reply, {:text, JsonResponse}, Req, state}
+    {:reply, {:text, jsonResponse}, req, state}
   end
 
   # get_markdown以外のメッセージの扱い
-  def websocket_handle({:text, Text}, Req, state) do
+  def websocket_handle({:text, text}, req, state) do
     room_id = state[:room_id]
-    {[{<<"set_markdown">>, RawMarkdown}, {<<"from">>, FromGuid}|_]} = :jiffy.decode(Text)
+    {[{<<"set_markdown">>, RawMarkdown}, {<<"from">>, fromGuid}|_]} = :jiffy.decode(text)
 
     markdown =
     if RawMarkdown === <<>> do
@@ -54,57 +54,57 @@ defmodule MessageWallHandler do
     :io.format("~w~n", [markdown])
     save_message(RoomId, markdown)
     # gprocにイベントを公開し、
-    # 全ての接続クライアントにwebsocket_info({gproc_ps_event, new_message, {RoomId, FromGuid}}, Req, State)を呼び出します
-    :gproc_ps.publish(:l, :new_message, {room_id, FromGuid})
-    {:ok, Req, state}
+    # 全ての接続クライアントにwebsocket_info({gproc_ps_event, new_message, {RoomId, fromGuid}}, req, State)を呼び出します
+    :gproc_ps.publish(:l, :new_message, {room_id, fromGuid})
+    {:ok, req, state}
   end
 
 
-  def websocket_handle({:binary, Data}, Req, state) do
-    {:reply, {:binary, Data}, Req, state}
+  def websocket_handle({:binary, data}, req, state) do
+    {:reply, {:binary, data}, req, state}
   end
-  def websocket_handle(_Frame, Req, state) do
-    {:ok, Req, state}
+  def websocket_handle(_Frame, req, state) do
+    {:ok, req, state}
   end
 
   # websocket_infoは本プロセスにErlangメッセージが届いた時に実行されます
   # gprocからnew_messageメッセージの場合はそのメッセージをWebSocketに送信します
-  def websocket_info({:gproc_ps_event, :new_message, {RoomId, FromGuid}}, Req, state) do
-    RawMessage = get_markdown(RoomId)
+  def websocket_info({:gproc_ps_event, :new_message, {roomId, fromGuid}}, req, state) do
+    rawMessage = get_markdown(roomId)
     # ETS結果をマップに変換
-    :io.format("~w", [RawMessage])
-    Message = format_message(RawMessage)
-    JsonResponse = :jiffy.encode(%{
-      <<"from">> => FromGuid,
+    :io.format("~w", [rawMessage])
+    message = format_message(rawMessage)
+    jsonResponse = :jiffy.encode(%{
+      <<"from">> => fromGuid,
       <<"type">> => <<"all">>,
-      <<"markdown">> => Message
+      <<"markdown">> => message
     })
-    {:reply, {:text, JsonResponse}, Req, state}
+    {:reply, {:text, jsonResponse}, req, state}
   end
-  def websocket_info(_Info, Req, state) do
-    {:ok, Req, state}
+  def websocket_info(_Info, req, state) do
+    {:ok, req, state}
   end
 
-  def websocket_terminate(_Reason, _Req, _State) do
+  def websocket_terminate(_reason, _req, _state) do
     :ok
   end
 
   # 対応するmarkdownを取得する
-  def get_markdown(Id) do
-    case :ets.lookup(:message_wall, Id) do
-      [] -> {Id, <<"">>}
+  def get_markdown(id) do
+    case :ets.lookup(:message_wall, id) do
+      [] -> {id, <<"">>}
       [Tuple] -> Tuple
     end
   end
 
   # ETS結果メッセージをJiffyが変換できる形式に変更
-  def format_markdown({_Id, Markdown}) do
-    Markdown
+  def format_markdown({_id, markdown}) do
+    markdown
   end
 
   # ETS結果メッセージをJiffyが変換できる形式に変更
-  def format_message({_Key, Markdown}) do
-    :unicode.characters_to_binary(Markdown)
+  def format_message({_key, markdown}) do
+    :unicode.characters_to_binary(markdown)
   end
 
   # IPタプルを文字列に変換
@@ -118,18 +118,18 @@ defmodule MessageWallHandler do
   #   io_lib:format("~4..0B-~2..0B-~2..0BT~2..0B:~2..0B:~2..0BZ", [Year, Month, Day, Hour, Minut, Second]).
 
   # ETSにメッセージを保存する
-  def save_message(Key, Markdown) do
-    :ets.insert(:message_wall, {Key, Markdown})
+  def save_message(key, markdown) do
+    :ets.insert(:message_wall, {key, markdown})
   end
 
   # IP取得
-  def get_ip(Req) do
+  def get_ip(req) do
     # プロキシ経由対応
-    case :cowboy_req.header(<<"x-real-ip">>, Req) do
-      {:undefined, _Req} ->
-        {{Ip, _Port}, _Req} = :cowboy_req.peer(Req)
+    case :cowboy_req.header(<<"x-real-ip">>, req) do
+      {:undefined, _req} ->
+        {{Ip, _port}, _req} = :cowboy_req.peer(req)
         Ip
-      {Ip, _Req} -> Ip
+      {Ip, _req} -> Ip
     end
   end
 end
